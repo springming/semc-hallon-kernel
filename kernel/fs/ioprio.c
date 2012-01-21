1b058c066171279e1ce2ac53dd7015049a593ef8
adridu59 <adri-from-59@hotmail.fr>
2012-01-21 13:31:21 +0100

    Revert "Fix IO Prio"
    This reverts commit 885370cc5220e6551a62bd0593ddc2ec8c7e5230.

--- kernel/fs/ioprio.c
+++ kernel/fs/ioprio.c
@@ -29,7 +29,7 @@
 
 int set_task_ioprio(struct task_struct *task, int ioprio)
 {
-	int i, err;
+	int err;
 	struct io_context *ioc;
 	const struct cred *cred = current_cred(), *tcred;
 
@@ -59,8 +59,6 @@ int set_task_ioprio(struct task_struct *task, int ioprio)
 			err = -ENOMEM;
 			break;
 		}
-		/* let other ioc users see the new values */
-		smp_wmb();
 		task->io_context = ioc;
 	} while (1);
 
