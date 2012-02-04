4fc5a843a62548751d88feb1794c0f86945e9443
adridu59 <adri-from-59@hotmail.fr>
2012-02-04 22:59:08 +0100

    cpuidle: menu: fixed wrapping timers at 4.294 seconds Cpuidle menu governor is using u32 as a temporary datatype for storing nanosecond values which wrap around at 4.294 seconds. This causes errors in predicted sleep times resulting in higher than should be C state selection and increased power consumption. This also breaks cpuidle state residency statistics.
    cc: stable@kernel.org # .32.x through .39.x
    Signed-off-by: Tero Kristo <tero.kristo@nokia.com>
    Signed-off-by: Len Brown <len.brown@intel.com>

--- kernel/drivers/cpuidle/governors/menu.c
+++ kernel/drivers/cpuidle/governors/menu.c
@@ -187,7 +187,7 @@ static int menu_select(struct cpuidle_device *dev)
 	unsigned int power_usage = -1;
 	int i;
 	int multiplier;
-
+	struct timespec t;
 	data->last_state_idx = 0;
 	data->exit_us = 0;
 
@@ -201,8 +201,9 @@ static int menu_select(struct cpuidle_device *dev)
 		return 0;
 
 	/* determine the expected residency time, round up */
+	t = ktime_to_timespec(tick_nohz_get_sleep_length());
 	data->expected_us =
-	    DIV_ROUND_UP((u32)ktime_to_ns(tick_nohz_get_sleep_length()), 1000);
+	    t.tv_sec * USEC_PER_SEC + t.tv_nsec / NSEC_PER_USEC;
 
 
 	data->bucket = which_bucket(data->expected_us);
