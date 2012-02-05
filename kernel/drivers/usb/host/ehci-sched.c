f00e6fd6fd6e057cf6da1612e7f7b17e92adc7fe
adridu59 <adri-from-59@hotmail.fr>
2012-02-05 10:42:25 +0100

    USB: EHCI: Remove dead code from ehci-sched.c The pre-release GCC-4.6 now correctly flags this code as dead.
    Signed-off-by: David Daney <ddaney@xxxxxxxxxxxxxxxxxx>

--- kernel/drivers/usb/host/ehci-sched.c
+++ kernel/drivers/usb/host/ehci-sched.c
@@ -1040,8 +1040,6 @@ iso_stream_put(struct ehci_hcd *ehci, struct ehci_iso_stream *stream)
 	 * not like a QH -- no persistent state (toggle, halt)
 	 */
 	if (stream->refcount == 1) {
-		int		is_in;
-
 		// BUG_ON (!list_empty(&stream->td_list));
 
 		while (!list_empty (&stream->free_list)) {
@@ -1068,7 +1066,6 @@ iso_stream_put(struct ehci_hcd *ehci, struct ehci_iso_stream *stream)
 			}
 		}
 
-		is_in = (stream->bEndpointAddress & USB_DIR_IN) ? 0x10 : 0;
 		stream->bEndpointAddress &= 0x0f;
 		if (stream->ep)
 			stream->ep->hcpriv = NULL;
